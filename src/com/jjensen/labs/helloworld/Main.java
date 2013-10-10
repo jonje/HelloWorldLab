cd1bcaf6167efab929f4200b3a93fe6d5d1ff18c
jjensen <haiyun211@gmail.com>
2013-10-10 08:37:51 -0600

    Added tag

--- src/com/jjensen/labs/helloworld/Main.java
+++ src/com/jjensen/labs/helloworld/Main.java
@@ -1,5 +1,10 @@
 package com.jjensen.labs.helloworld;
 
+/**
+ * Hello World Application
+ * Created by: Jonathan Jensen
+ * @author jjensen
+ */
 public class Main {
 
     public static void main(String[] args) {
